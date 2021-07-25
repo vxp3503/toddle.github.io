c18e7e01a4856c019ccb7e689d24199da5070fd5
vxp3503 <vikrantverma725@gmail.com>
2021-07-25 18:54:06 +0530

    Done

--- toddle.js
+++ toddle.js
@@ -35,7 +35,7 @@ function js()
         
         d.appendChild(box);
         d.appendChild(input);
-        d.style.color="green";
+        d.style.color="blue";
         var line=document.createElement('hr');
         line.style.width="800px"
         col2.appendChild(d);
@@ -66,7 +66,7 @@ function js()
         arrowLeft.onclick=function()
         {
             console.log("Left Click")
-            input.style.color="green";
+            input.style.color="blue";
             d.style.marginLeft="70px";
         }
         arrows.onclick=function()
